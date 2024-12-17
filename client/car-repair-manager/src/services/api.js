2c4da68a7f1503c3862a061eb2729237d50f5e33
Samuel <samadeolu7@gmail.com>
2024-12-17 11:16:20 +0100

    change routes to match server

--- client/car-repair-manager/src/services/api.js
+++ client/car-repair-manager/src/services/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // Replace with your backend URL
+    baseURL: 'http://localhost:5000', 
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
-export const getCars = () => api.get('/cars');
-export const getCarById = (id) => api.get(`/cars/${id}`);
-export const createCar = (car) => api.post('/cars', car);
-export const updateCar = (id, car) => api.put(`/cars/${id}`, car);
+export const getCars = () => api.get('/car/');
+export const getCarById = (id) => api.get(`/car/${id}`);
+export const createCar = (car) => api.post('/car/', car);
+export const updateCar = (id, car) => api.put(`/car/${id}`, car);
 export const generatePdf = (data) => api.post('/generate-pdf', data);
 
 export const repairCar = (id, data) => api.put(`/repair/add-repair-details`, data);
